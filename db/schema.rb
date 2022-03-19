b09856b1a68563138d1c2168980ffe85468f999c
Chloe <chloepeng331@icloud.com>
2022-03-18 21:50:05 -0400

    commit tree bc6e8f3f712b59bc2f533f00f1a949b3f8943c6e

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2022_03_19_012751) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
